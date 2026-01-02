deb21e856824adc2f9ebdabba3fbec46a3f4d7ca
rashel9255 <ibrahim2000167@gmail.com>
2026-01-02 14:29:20 +0600

    feat: create nav menu and add actice effect

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import Logo from '../ui/Logo';
+import NavLink from '../ui/NavLink';
 
 const Navbar = () => {
+
+    const navMenu = (
+        <>
+            <li>
+                <NavLink href={"/"}>Home</NavLink>
+            </li>
+            <li>
+                <NavLink href={"/products"}>Products</NavLink>
+            </li>
+            <li>
+                <NavLink href={"/about"}>About</NavLink>
+            </li>
+            <li>
+                <NavLink href={"/contact"}>Contact</NavLink>
+            </li>
+        </>
+    )
+
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
@@ -13,48 +32,14 @@ const Navbar = () => {
                         </svg>
                     </div>
                     <ul tabIndex="-1" className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a>Item 1</a>
-                        </li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li>
-                                    <a>Submenu 1</a>
-                                </li>
-                                <li>
-                                    <a>Submenu 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a>Item 3</a>
-                        </li>
+                        {navMenu}
                     </ul>
                 </div>
                 <Logo />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <a>Item 1</a>
-                    </li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 w-40 z-1">
-                                <li>
-                                    <a>Submenu 1</a>
-                                </li>
-                                <li>
-                                    <a>Submenu 2</a>
-                                </li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li>
-                        <a>Item 3</a>
-                    </li>
+                    {navMenu}
                 </ul>
             </div>
             <div className="navbar-end">
---
+++ src/Components/ui/NavLink.jsx
@@ -0,0 +1,17 @@
+'use client'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const NavLink = ({ children, href }) => {
+    const pathname = usePathname();
+    const isActive = pathname === href;
+
+    return (
+        <Link href={href} className={isActive ? 'text-primary font-bold' : ''}>
+            {children}
+        </Link>
+    );
+};
+
+export default NavLink;
